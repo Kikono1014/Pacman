3bd7f829da1d58c70026047160d81ed22e76eebe
Kikono1014 <vitalik.pyvovarchuk@gmail.com>
2025-03-16 13:10:17 +0200

    added sprite rendering to main

--- sprite.py
+++ sprite.py
@@ -3,5 +3,5 @@ import pygame
 
 class Sprite:
     def __init__(self, surface : pygame.Surface, rect : pygame.Rect):
-        self.texture : pygame.Surface = surface
+        self.texture : pygame.Surface = surface.subsurface(rect)
         self.area : pygame.Rect = rect
