c2e563af48dacd314b831c74ccfff71cde090394
Andrii Volkov <qwerttyuiop050@ukr.net>
2025-04-03 15:59:12 +0300

    add def set_direction

--- pacman.py
+++ pacman.py
@@ -1,15 +1,14 @@
-import pygame
+п»їimport pygame
 from moveable import Moveable
 from arena import Dot
 
-class ClassName(object):
+class Pacman(Moveable):
     def __init__(self, sprites, position, direction, speed, arena):
         super().__init__(sprites, position, direction, speed)
         self.arena = arena
         self.next_direction = direction
 
     def update_destination(self):
-        """Обновляет направление, если возможно, и двигает Pac-Man."""
         if self.can_move(self.next_direction):
             self.direction = self.next_direction 
         
@@ -17,11 +16,24 @@ class ClassName(object):
             super().update_destination()
 
     def can_move(self, direction):
-        """Проверяет, можно ли двигаться в заданном направлении."""
         new_x = (self.position[0] + direction[0]) % len(self.arena.map[0])
         new_y = (self.position[1] + direction[1]) % len(self.arena.map)
         return self.arena.map[new_y][new_x] != Dot.WALL
 
     def rotate(self, direction):
-        """Запоминает следующее направление движения."""
         self.next_direction = direction
+
+    def move(self, map):
+        next_pos = ((self.position[0] + self.direction[0]) % len(map[0]), 
+                    (self.position[1] + self.direction[1]) % len(map))
+
+        if map[next_pos[1]][next_pos[0]] != Dot.WALL:
+            self.position = next_pos
+            self.arena.remove_dot(self.position)
+
+    def set_direction(self, new_direction):
+        next_pos = ((self.position[0] + new_direction[0]) % len(self.arena.map[0]),
+                    (self.position[1] + new_direction[1]) % len(self.arena.map))
+
+        if self.arena.map[next_pos[1]][next_pos[0]] != Dot.WALL:
+            self.direction = new_direction
